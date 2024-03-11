7a7cbf2b4026c87cc5b0ece364b6ac5045f896f7
Dessire1012 <dessire@unitec.edu>
2024-03-11 00:35:22 -0600

    Función tiempo + excel

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Tarea 2: Algoritmos de ordenamiento\n");
 
-        int tamaño = 100000;
+        int tamaño = 10000;
         int[] arr = new int[tamaño];
 
         Random rand = new Random();
@@ -22,65 +22,62 @@ public class Main {
         }
         System.out.println(" }");
 
-        long startTime;
-        long endTime;
+        long startTime = System.nanoTime();
+        long endTime = System.nanoTime();
 
         // Insertion sort
         Insertion_sort ins_sort = new Insertion_sort();
-        long startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         ins_sort.sort(arr);
-        long endTime = System.currentTimeMillis();
+        endTime = System.nanoTime();
         System.out.println("\nInsertion sort:");
         ins_sort.printArray(arr);
-        System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+        System.out.println("Tiempo de ejecución: " + ((endTime - startTime) / 1e6) + " milisegundos");
 
         // Selection sort
         Selection_sort select_sort = new Selection_sort();
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         select_sort.sort(arr);
-        endTime = System.currentTimeMillis();
+        endTime = System.nanoTime();
         System.out.println("\nSelection sort:");
         select_sort.printArray(arr);
-        System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
-
+        System.out.println("Tiempo de ejecución: " + ((endTime - startTime) / 1e6) + " milisegundos");
 
         // Bubble sort
         Bubble_sort bubble_sort = new Bubble_sort();
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         bubble_sort.sort(arr);
-        endTime = System.currentTimeMillis();
+        endTime = System.nanoTime();
         System.out.println("\nBubble sort:");
         bubble_sort.printArray(arr);
-        System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
-
+        System.out.println("Tiempo de ejecución: " + ((endTime - startTime) / 1e6) + " milisegundos");
 
         // Merge sort
         Merge_sort mergeSort = new Merge_sort();
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         mergeSort.sort(arr, 0, tamaño - 1);
-        endTime = System.currentTimeMillis();
+        endTime = System.nanoTime();
         System.out.println("\nMerge sort:");
         mergeSort.printArray(arr);
-        System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+        System.out.println("Tiempo de ejecución: " + ((endTime - startTime) / 1e6) + " milisegundos");
 
         // Heap sort
         Heap_sort heapSort = new Heap_sort();
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         heapSort.sort(arr);
-        endTime = System.currentTimeMillis();
+        endTime = System.nanoTime();
         System.out.println("\nHeap sort");
         heapSort.printArray(arr);
-        System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
+        System.out.println("Tiempo de ejecución: " + ((endTime - startTime) / 1e6) + " milisegundos");
 
         // Quick Sort
         int N = arr.length;
         Quick_sort quickSort = new Quick_sort();
-        startTime = System.currentTimeMillis();
+        startTime = System.nanoTime();
         quickSort.quickSort(arr, 0, N - 1);
-        endTime = System.currentTimeMillis();
+        endTime = System.nanoTime();
         System.out.println("\nQuick sort:");
         quickSort.printArr(arr);
-        System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos");
-
+        System.out.println("Tiempo de ejecución: " + ((endTime - startTime) / 1e6) + " milisegundos");
     }
 }
